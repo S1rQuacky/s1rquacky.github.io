a0865aa781475aa705c0bef0c7a3d3ec2f825563
Neide Urbina <neide.n.urbina@gmail.com>
2021-08-30 20:28:59 -0500

    Third project push

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-let recipeData
+//let recipeData
 
-const $label=$('#label');
+let $label=$('#label');
+//let $result=$('#result');
 const $input=$('input[type="text"]');
 
 function handleGetData(event) {
@@ -9,11 +10,13 @@ function handleGetData(event) {
         url: `https://api.edamam.com/api/recipes/v2?type=public&q=${$input.val()}&app_id=94686c3b&app_key=%20682a1a1e2dd05346d1bcd667e867ed16%09`
         //${$input.val()}
 
+
     }).then(
         function(data){
             //recipeData = JSON.parse(data.hits)
             for (let i = 0; i < data.hits.length; i++){
-                $label.text(data.hits[i].recipe.label)
+               $label.text(data.hits[i].recipe.label)
+               
             }
             //$label.text(data.hits[0].recipe.label);
 
